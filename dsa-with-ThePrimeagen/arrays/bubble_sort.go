1c1c30ce87b2d1e95b938619d23afb250362cd38
LuanTran <tranluanqqq@gmail.com>
2024-07-16 22:43:32 +0700

    feat(dsa-arr): bubble sort

--- dsa-with-ThePrimeagen/arrays/bubble_sort.go
+++ dsa-with-ThePrimeagen/arrays/bubble_sort.go
@@ -2,9 +2,9 @@ package arrays
 
 func BubbleSort(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr); j++ {
-			if arr[i] < arr[j] {
-				arr[j], arr[i] = arr[i], arr[j]
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j+1] < arr[j] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
 	}
