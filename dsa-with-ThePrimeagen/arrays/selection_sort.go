1b262a6525f97323f0b8d23e3306c719011d0180
LuanTran <tranluanqqq@gmail.com>
2024-07-19 21:36:02 +0700

    feat(dsa-arr): selection sort

---
+++ dsa-with-ThePrimeagen/arrays/selection_sort.go
@@ -0,0 +1,15 @@
+package arrays
+
+func SelectionSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		minIdx := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+
+		arr[i], arr[minIdx] = arr[minIdx], arr[i]
+	}
+	return arr
+}
