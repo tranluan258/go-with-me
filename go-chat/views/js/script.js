f61030b5cccae537b59b610c50fbe7d977f6fb49
LuanTran <tranluanqqq@gmail.com>
2024-07-24 22:05:20 +0700

    feat(hx-on): prepend new room after request

--- go-chat/views/js/script.js
+++ go-chat/views/js/script.js
@@ -153,3 +153,39 @@ function toogleDropdown() {
   dropdownMenu.style.display =
     dropdownMenu.style.display === "block" ? "none" : "block";
 }
+
+function prependNewRoom() {
+  // @ts-ignore
+  document.querySelector(".modal-backdrop")?.click();
+  const listRoom = document.querySelector(".list-room");
+  const roomId = document.querySelector(".room_id_header")?.id;
+  let isExisted = false;
+
+  listRoom?.childNodes.forEach((node) => {
+    if (node instanceof HTMLElement && node.id === roomId) {
+      isExisted = true;
+      return;
+    }
+  });
+
+  if (isExisted) return;
+
+  const newRoom = `
+<li
+  id='${roomId}'
+  class='flex items-center w-full h-[70px] mb-2 p-4 rounded-xl cursor-pointer hover:bg-gray-50'
+  hx-get='messages?room_id={{.ID}}'
+  hx-target='#chat-container'
+  hx-swap='innerHTML'
+  onclick='connectWs(event)'
+>
+  <div class='avatar placeholder'>
+    <div class='bg-neutral text-neutral-content w-10 rounded-full'>
+      <span class='text-xs'>UNE</span>
+    </div>
+  </div>
+  <span class='ml-3 truncate'>Test</span>
+</li>
+`;
+  listRoom?.insertAdjacentHTML("beforebegin", newRoom);
+}
