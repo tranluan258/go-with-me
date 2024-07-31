29f1bdaa9589ba6304875b9769733f5093ef44ff
LuanTran <tranluanqqq@gmail.com>
2024-07-31 22:42:40 +0700

    feat(ws): update ws message sent to client

--- go-chat/internal/handlers/message_handler.go
+++ go-chat/internal/handlers/message_handler.go
@@ -28,7 +28,7 @@ func (mh *MessageHandler) GetMessagesByRoom(ctx echo.Context) error {
 
 	var messages []models.Message
 
-	err := mh.db.Select(&messages, "SELECT id,message,sender_id,full_name,created_time FROM messages WHERE room_id=$1 ORDER BY created_time DESC", roomId)
+	err := mh.db.Select(&messages, "SELECT id,message,sender_id,full_name,created_time FROM messages WHERE room_id=$1 ORDER BY created_time ASC", roomId)
 	if err != nil {
 		return err
 	}
@@ -57,6 +57,7 @@ func (mh *MessageHandler) GetMessagesByRoom(ctx echo.Context) error {
       AND 
           r.id=$2;
       `, cookie.Value, roomId)
+	ctx.Response().Header().Set("Vary", "HX-Request")
 	return ctx.Render(http.StatusOK, "messages", map[string]interface{}{
 		"Messages": messages,
 		"UserId":   cookie.Value,
