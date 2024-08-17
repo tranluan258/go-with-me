ab5499eb70bd58c3361b15ad19d7bc2640195862
LuanTran <tranluanqqq@gmail.com>
2024-08-17 22:28:56 +0700

    dsa: bst delete func

--- dsa-with-ThePrimeagen/tree/bst.go
+++ dsa-with-ThePrimeagen/tree/bst.go
@@ -54,3 +54,99 @@ func (t *BST) find(val int) bool {
 	}
 	return false
 }
+
+func (t *BST) delete(val int) {
+	t.length--
+	curr := t.tree
+	var parent *BinaryNode
+
+	for curr != nil {
+		if curr.val == val {
+			if curr.left == nil && curr.right == nil {
+				t.removeNodeDontHaveLeftRight(curr, parent)
+				break
+			} else if curr.left != nil && curr.right != nil {
+				t.removeNodeHaveBothLeftRight(curr, parent)
+				break
+			} else {
+				t.removeNodeOnlyHaveLeftOrRight(curr, parent)
+				break
+			}
+		}
+
+		parent = curr
+		if val > curr.val {
+			curr = curr.right
+		} else if val < curr.val {
+			curr = curr.left
+		}
+
+	}
+}
+
+func (t *BST) removeNodeDontHaveLeftRight(curr *BinaryNode, parent *BinaryNode) {
+	if curr.val > parent.val {
+		parent.right = nil
+	} else {
+		parent.left = nil
+	}
+	curr = nil
+}
+
+func (t BST) removeNodeHaveBothLeftRight(curr *BinaryNode, parent *BinaryNode) {
+	tmp := curr
+	var prev *BinaryNode
+
+	if curr.val > parent.val {
+		for curr.left != nil {
+			prev = curr
+			curr = curr.left
+		}
+
+		if prev != nil {
+			prev.left = nil
+		}
+
+		parent.right = curr
+		curr.right = tmp.right
+
+	} else {
+
+		for curr.right != nil {
+			prev = curr
+			curr = curr.right
+		}
+
+		if prev != nil {
+			prev.left = nil
+		}
+
+		parent.left = curr
+		curr.left = tmp.left
+	}
+	return
+}
+
+func (t *BST) removeNodeOnlyHaveLeftOrRight(curr *BinaryNode, parent *BinaryNode) {
+	if curr.val > parent.val {
+		if curr.left == nil {
+			parent.right = curr.right
+			curr = nil
+		} else if curr.right == nil {
+			parent.right = curr.left
+			curr = nil
+		}
+		return
+	}
+
+	if curr.val < parent.val {
+		if curr.left == nil {
+			parent.left = curr.right
+			curr = nil
+		} else if curr.right == nil {
+			parent.left = curr.left
+			curr = nil
+		}
+		return
+	}
+}
--- dsa-with-ThePrimeagen/tree/bst_test.go
+++ dsa-with-ThePrimeagen/tree/bst_test.go
@@ -10,9 +10,10 @@ func TestBst(t *testing.T) {
 
 	tree.insert(2)
 	tree.insert(1)
+	tree.insert(4)
 	tree.insert(3)
 	tree.insert(5)
-	tree.insert(4)
+	tree.insert(7)
 
 	if !tree.find(3) {
 		t.Fatalf("expected %t rec %t", true, false)
@@ -21,6 +22,29 @@ func TestBst(t *testing.T) {
 		t.Fatalf("expected %t rec %t", true, false)
 	}
 	if tree.find(6) {
+		t.Fatalf("expected %t rec %t", false, true)
+	}
+
+	tree.delete(4)
+	if tree.find(4) {
+		t.Fatalf("expected %t rec %t", false, true)
+	}
+
+	if tree.tree.right.val != 3 {
+		t.Fatalf("expected %d rec %d", 3, tree.tree.right.val)
+	}
+
+	if !tree.find(5) {
+		t.Fatalf("expected %t rec %t", true, false)
+	}
+
+	tree.delete(5)
+	if tree.find(5) {
+		t.Fatalf("expected %t rec %t", false, true)
+	}
+
+	tree.delete(7)
+	if tree.find(7) {
 		t.Fatalf("expected %t rec %t", false, true)
 	}
 }
